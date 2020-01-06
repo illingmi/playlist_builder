29d356fe047c67d8604d2872933ea6cc3ae6d27f
illingmi <49622363+illingmi@users.noreply.github.com>
2020-01-05 20:46:04 -0800

    Add files via upload
    Added basics of find search results function

--- playlistBuilder.py
+++ playlistBuilder.py
@@ -11,15 +11,6 @@ username = sys.argv[1]
 # Fix scope maybe
 scope = 'user-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private'
 
-# USER ID: mibaeoch3ierdvfq3g2wdvvl5
-# PLAYLIST ID: <your playlist id>
-
-# USER AUTHENTICATION COMMANDS:
-    # $env:SPOTIPY_CLIENT_ID="26fd947cd19549f0a90ecaed2aab905d"
-    # $env:SPOTIPY_CLIENT_SECRET="46402d169e204898a6f7cb6883bffd73"
-    # $env:SPOTIPY_REDIRECT_URI="http://google.ca/"
-
-# Erase cache and prompt for user permission
 try:
     token = util.prompt_for_user_token(username, scope)
 except:
@@ -33,53 +24,67 @@ description = "The King and Queen had 2 daughters. Their names were Olivia and E
 words = ['The', 'King', 'And', 'Queen', 'Had', '2', 'Daughters', 'Their', 'Names',
           'Were', 'Olivia', 'And', 'Emerald']
 
-# Finds all the potential songs searches up to five words
-def findPotentialSearches():
-    # if array of words is not empty
-    if words:
-        search1 = words[0]
-        search2 = words[0] + " " + words[1]
-        search3 = words[0] + " " + words[1] + " " + words[2]
-        search4 = words[0] + " " + words[1] + " " + words[2] + " " + words[3]
-        search5 = words[0] + " " + words[1] + " " + words[2] + " " + words[3] + " " + words[4]
+#print(json.dumps(name, sort_keys=True, indent=4))
+searches = []
+potentialSongs = []
 
-def findPotentialSongs():
-    potentials1 = []
-    potentials2 = []
-    potentials3 = []
-    potentials4 = []
-    potentials5 = []
-    #for (i = 1; i <= 5; i++)
-    results1 = spotify.search(q='track:' + search1)
+def findSearchResults(array):
+    for i in range(4):
+        # if array of words is not empty --> need a different way to write this
+        #if array:
+        # Finds all the potential songs searches up to five words
+        if i == 0:
+            search = array[i]
+            prevSearch = search
+            searches.append(search)
+        else:
+            search = prevSearch + " " + array[i]
+            prevSearch = search
+            searches.append(search)
 
-    '''first = results1['tracks']['items'][0]['album']['name']
-    sec = results1['tracks']['items'][1]['album']['name']
-    third = results1['tracks']['items'][2]['album']['name']
-    forth = results1['tracks']['items'][3]['album']['name']
+        # get search results
+        temp = []
+        results = spotify.search(q='track:' + search, type='track')
+        index = len(results['tracks']['items'])
+        for x in range(index):
+            name = results['tracks']['items'][x]['album']['name']
+            temp.append(name)
+
+        potentialSongs.append(temp)
+    print(searches)
+    print(potentialSongs)
+
+test = ['The', 'King', 'And', 'Queen']
+findSearchResults(test)
+print(potentialSongs[0][0])
+#potentialSongs[0].find(searches[0])
+print(searches[0])
 
 
-    #print(json.dumps(name, sort_keys=True, indent=4))
-    potentials1.extend([name1, name2, name3, name4])
-    print(potentials1)
-    '''
+#results1 = spotify.search(q='track:The King')
+#print(results1)
+test = []
 
-if words:
-    search1 = words[0]
-    search2 = words[0] + " " + words[1]
-    search3 = words[0] + " " + words[1] + " " + words[2]
-    search4 = words[0] + " " + words[1] + " " + words[2] + " " + words[3]
-    search5 = words[0] + " " + words[1] + " " + words[2] + " " + words[3] + " " + words[4]
+results = spotify.search(q='track:The King', type='track')
+#print(json.dumps(results, sort_keys=True, indent=4))
 
-print(search1)
-print(search2)
-print(search3)
-print(search4)
-print(search5)
-print(words)
+name1 = results['tracks']['items'][0]['album']['name']
+name2 = results['tracks']['items'][1]['album']['name']
+name3 = results['tracks']['items'][2]['album']['name']
+name4 = results['tracks']['items'][3]['album']['name']
+name5 = results['tracks']['items'][4]['album']['name']
+name6 = results['tracks']['items'][5]['album']['name']
+name7 = results['tracks']['items'][6]['album']['name']
+name8 = results['tracks']['items'][7]['album']['name']
+none = results['tracks']['items'][9]['album']['name']
 
-i = 1
-results + str(1)
+test.append([name1, name2, name3, name4, name5, name6, name7, name8])
+#print("Hard Coded:")
+#print(test)
 
+
+
+'''
 def findClosestPlaylist():
     # picks the playlist that most accurately matches the description
 
@@ -87,3 +92,4 @@ def findClosestPlaylist():
         name = ' '.join(sys.argv[1:])
     else:
         name = 'Radiohead'
+'''
